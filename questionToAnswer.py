149e57db465a1199cb526b2200fb73c38b1f08ae
Salomon-Novachrono <[hinterberger.thomas@sus.htl-grieskirchen.at]>
2021-01-02 21:18:25 +0100

    push it

--- questionToAnswer.py
+++ questionToAnswer.py
@@ -1,21 +1,21 @@
 import numpy as np # Math
 from markdown import markdown
 from simpletransformers.question_answering import QuestionAnsweringModel
-
+import wikipedia
 model = QuestionAnsweringModel('distilbert', 'distilbert-base-uncased-distilled-squad',use_cuda=False)
 
 
 question_data = {
   'qas': 
-  [{'question': 'When was bill gates bron',
+  [{'question': 'When was Ubuntu released',
     'id': 0,
     'answers': [{'text': ' ', 'answer_start': 0}],
     'is_impossible': False}],
-  'context': 'Bill gates was a very popular man. He was back in 1967 and had two parents'
+  'context': wikipedia.summary("Ubuntu")
   }
 try:
   prediction = model.predict([question_data])
 except:
   print("")
 
-print(prediction[0])
+print(prediction)
